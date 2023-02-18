3fdd1a10e9f695aecc931317e1d1a0781bb3fcdb
deniskhan22bd <odinstrelok02@gmail.com>
2023-02-18 14:03:59 +0600

    changes

--- tsis_4/json/json_sample.py
+++ tsis_4/json/json_sample.py
@@ -8,7 +8,13 @@ print("""Interface Status
 ================================================================================
 DN                                                 Description           Speed    MTU  
 -------------------------------------------------- --------------------  ------  ------""")
-print(data["imdata"][0]["l1PhysIf"]["attributes"]["dn"]," " * 28 ,data["imdata"][0]["l1PhysIf"]["attributes"]["speed"], " " , data["imdata"][0]["l1PhysIf"]["attributes"]["mtu"])
-print(data["imdata"][1]["l1PhysIf"]["attributes"]["dn"]," " * 28 ,data["imdata"][1]["l1PhysIf"]["attributes"]["speed"], " " , data["imdata"][1]["l1PhysIf"]["attributes"]["mtu"])
-print(data["imdata"][2]["l1PhysIf"]["attributes"]["dn"]," " * 28 ,data["imdata"][2]["l1PhysIf"]["attributes"]["speed"], " " , data["imdata"][2]["l1PhysIf"]["attributes"]["mtu"])
+
+
+for i in data["imdata"]:
+    print(i["l1PhysIf"]["attributes"]["dn"], " " * 28, i["l1PhysIf"]["attributes"]["speed"], " ", i["l1PhysIf"]["attributes"]["mtu"])
+
+
+#print(data["imdata"][0]["l1PhysIf"]["attributes"]["dn"]," " * 28 ,data["imdata"][0]["l1PhysIf"]["attributes"]["speed"], " " , data["imdata"][0]["l1PhysIf"]["attributes"]["mtu"])
+#print(data["imdata"][1]["l1PhysIf"]["attributes"]["dn"]," " * 28 ,data["imdata"][1]["l1PhysIf"]["attributes"]["speed"], " " , data["imdata"][1]["l1PhysIf"]["attributes"]["mtu"])
+#print(data["imdata"][2]["l1PhysIf"]["attributes"]["dn"]," " * 28 ,data["imdata"][2]["l1PhysIf"]["attributes"]["speed"], " " , data["imdata"][2]["l1PhysIf"]["attributes"]["mtu"])
 
